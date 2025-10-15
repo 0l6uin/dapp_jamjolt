9453e2309286dbb3f0bbcd055524e5921ed009f3
mario <196494778+0l6uin@users.noreply.github.com>
2025-10-15 17:33:57 -0300

    Update PageRoutes.tsx

--- frontend/src/PageRoutes.tsx
+++ frontend/src/PageRoutes.tsx
@@ -4,7 +4,6 @@ import TrainPage from './TrainPage/trainpage';
 import RecPage from './RecPage/recpage';
 import PostPage from './PostPage/postpage';
 import RankPage from './RankPage/rankpage';
-import ValidationKey from './validation-key.txt';
 //import Navbar from './Navbar';
 
 const PageRoutes: React.FC = () => {
@@ -17,7 +16,6 @@ const PageRoutes: React.FC = () => {
           <Route path ="/recpage" element={<RecPage />} />
           <Route path ="/postpage" element={<PostPage />} />
           <Route path ="/ranking" element={<RankPage />} />
-          <Route path ="/validation-key.txt" element={<ValidationKey />} />
           <Route path ="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
       </div>
