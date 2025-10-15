b580151cb9fde60db4add1b9baed56aef974596e
mario <196494778+0l6uin@users.noreply.github.com>
2025-10-15 17:17:06 -0300

    Add route for validation key page

--- frontend/src/PageRoutes.tsx
+++ frontend/src/PageRoutes.tsx
@@ -4,6 +4,7 @@ import TrainPage from './TrainPage/trainpage';
 import RecPage from './RecPage/recpage';
 import PostPage from './PostPage/postpage';
 import RankPage from './RankPage/rankpage';
+import ValidationKey from './validation-key.txt';
 //import Navbar from './Navbar';
 
 const PageRoutes: React.FC = () => {
@@ -16,6 +17,7 @@ const PageRoutes: React.FC = () => {
           <Route path ="/recpage" element={<RecPage />} />
           <Route path ="/postpage" element={<PostPage />} />
           <Route path ="/ranking" element={<RankPage />} />
+          <Route path ="/validation-key.txt" element={<ValidationKey />} />
           <Route path ="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
       </div>
